65b02a0fbcc6dec0757b43407da8e0b0a64659f1
Adeinr22 <andrei05.jules22@gmail.com>
2026-05-26 01:22:09 +0800

    Update question_2.py

--- Source Codes/question_2.py
+++ Source Codes/question_2.py
@@ -4,11 +4,10 @@ nobel_dataframe = pd.read_csv('nobel.csv')
 
 def highest_usa_winner_decade(df) -> int | None:
     df['decade'] = (df['year'] // 10) * 10
-    total_decade = df.groupby('decade').size()
-    usa_born = df[df['birth_country'] == 'United States of America']
-    usa_decade = usa_born.groupby('decade').size()
-    ratio = (usa_decade / total_decade)
-    return ratio.idxmax()
+    df['is_usa_born'] = df['birth_country'] == 'United States of America'
+    decade_ratios = df.groupby('decade')['is_usa_born'].mean()
+    return int(decade_ratios.idxmax())
+max_decade_usa: int = highest_usa_winner_decade(nobel_dataframe)
 
 max_decade_usa: int = highest_usa_winner_decade(nobel_dataframe)
 print("=" * 110)
